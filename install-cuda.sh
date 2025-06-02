5a90b43d3ac03c9ebdb634eee833101dab42a02c
kasisdism <kasisdism@arv.co.th>
2025-06-02 16:46:42 +0700

    update script

--- install-cuda.sh
+++ install-cuda.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# # Install the nvidia-driver-<version>
-# echo 'Install nvidia-driver'
-# sudo apt install nvidia-driver-570
+# Install the nvidia-driver-<version>
+echo 'Install nvidia-driver'
+sudo apt install nvidia-driver-570
 
-# # Install the cuda-keyring package:
-# echo 'Install cuda-keyring'
-# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
-# sudo dpkg -i cuda-keyring_1.1-1_all.deb
+# Install the cuda-keyring package:
+echo 'Install cuda-keyring'
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
 
-# #Install CUDA SDK
-# echo 'Install CUDA SDK'
-# sudo apt-get update
-# sudo apt-get -y install cuda-toolkit-12-9
+#Install CUDA SDK
+echo 'Install CUDA SDK'
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-9
 
-# # Set up CUDA environment variables (for current user)
-# echo 'Update CUDA PATH'
-# {
-#   echo ''
-#   echo '# CUDA environment'
-#   echo 'export PATH=${PATH}:/usr/local/cuda-12.9/bin'
-#   echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-12.9/lib64'
-# } >> ~/.bashrc
+# Set up CUDA environment variables (for current user)
+echo 'Update CUDA PATH'
+{
+  echo ''
+  echo '# CUDA environment'
+  echo 'export PATH=${PATH}:/usr/local/cuda-12.9/bin'
+  echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-12.9/lib64'
+} >> ~/.bashrc
 
 # Install nvidia-container-runtime
 echo 'Install nvidia-container-runtime'
